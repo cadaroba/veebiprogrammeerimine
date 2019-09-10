b80d99003a06a77ce2020c3093e8e018970a57e4
Robin Kadakas <cadaroba@users.noreply.github.com>
2019-09-10 11:09:21 +0300

    Esimene üleslaadimine

---
+++ 02tund/testpage.php
@@ -0,0 +1,22 @@
+<?php
+  $userName = "Robin";
+?>
+<!DOCTYPE HTML>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+	echo $userName;
+  ?>
+  Esimene test</title>
+
+</head>
+<body>
+<?php
+  echo "<h1>" . $userName;
+?>
+  <h1>Esimene test</h1>
+  <p>Veebileht on loodud õppetöö käigus, ei sisalda tõsiselt võetavat sisu!</p>
+</body>
+</html>
