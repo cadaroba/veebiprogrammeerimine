218b44581488737781a7f50f1fcdff889e1e2663
Robin Kadakas <cadaroba@users.noreply.github.com>
2019-09-10 11:34:51 +0300

    Lisatud kellaaja kontorllimine

--- 02tund/testpage.php
+++ 02tund/testpage.php
@@ -1,5 +1,12 @@
 <?php
   $userName = "Robin";
+  $fullTimeNow = date("d.m.Y H:i:s"); #muutuja on $
+  $hourNow = date ("H");
+  $partOfDay = "hägune aeg";
+  
+  if ($hourNow < 8){
+	  $partOfDay = "hommik";
+  }
 ?>
 <!DOCTYPE HTML>
 <html lang="et">
@@ -18,5 +25,9 @@
 ?>
   <h1>Esimene test</h1>
   <p>Veebileht on loodud õppetöö käigus, ei sisalda tõsiselt võetavat sisu!</p>
+  <hr>
+  <?php
+    echo "<p>Lehe avamise hetkel oli aeg: " .$fullTimeNow .", ".$partOfDay."</p>";
+  ?>
 </body>
 </html>
